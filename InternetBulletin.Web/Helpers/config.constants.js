3dd731e2482af6fcc9894b24ba1c70b53d50f794
Debanjan Paul <debanjanpaul10@gmail.com>
2025-05-18 14:52:59 +0530

    removed auth0 constants

--- InternetBulletin.Web/Helpers/config.constants.js
+++ InternetBulletin.Web/Helpers/config.constants.js
@@ -6,9 +6,3 @@ export const UrlConstants = {
 	AppWebUrl:
 		"https://app-webapi-internet-bulletin-g8c0dba7c9gxgchc.centralindia-01.azurewebsites.net",
 };
-export const Auth0Constants = {
-	Domain: "debanjans-lab.uk.auth0.com",
-	ClientId: "Tz6visNGgDgv7cic60Bt8l2xDQONKw0W",
-	Audience: "24807da0-9de4-46b0-936a-df19f1a400ab",
-	Scope: "read:current_user update:current_user_metadata",
-};
