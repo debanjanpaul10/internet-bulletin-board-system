981abbc8fee965331b16c1b2763b08d9739b2c0d
Debanjan Paul <debanjanpaul10@gmail.com>
2025-05-24 14:12:32 +0530

    added jsdoc

--- InternetBulletin.Web/Components/Profile/Components/UserRatings/index.jsx
+++ InternetBulletin.Web/Components/Profile/Components/UserRatings/index.jsx
@@ -16,6 +16,32 @@ import { MyProfilePageConstants } from "@helpers/ibbs.constants";
 import { SparkleCircle28Regular } from "@fluentui/react-icons";
 import { formatDate } from "@helpers/common.utility";
 
+/**
+ * UserRatingsComponent displays a user's post ratings in a card-based table layout.
+ * The component shows a list of posts that the user has rated, including the post name
+ * and the date when the rating was given. Each post entry is displayed with a sparkle icon
+ * and formatted date.
+ * 
+ * @component
+ * @param {Object} props - Component props
+ * @param {Array<Object>} props.userPostRatings - Array of user's post ratings
+ * @param {string} props.userPostRatings[].postName - Name of the rated post
+ * @param {string} props.userPostRatings[].ratedOn - Date when the post was rated (will be formatted using formatDate utility)
+ * 
+ * @example
+ * // Example usage:
+ * <UserRatingsComponent 
+ *   userPostRatings={[
+ *     { postName: "Sample Post", ratedOn: "2024-03-20" }
+ *   ]} 
+ * />
+ * 
+ * @returns {JSX.Element} A card containing a table with the following features:
+ *  - A header showing "Your Ratings"
+ *  - A table with columns for post name and rating date
+ *  - Each row displays a sparkle icon, post name, and formatted date
+ *  - Scrollable content area for multiple ratings
+ */
 function UserRatingsComponent({ userPostRatings }) {
 	const styles = useStyles();
 
