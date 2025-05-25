b5047361b7860f56949ca7fbbb24cd50b8cbeb04
Debanjan Paul <debanjanpaul10@gmail.com>
2025-05-25 22:54:34 +0530

    Updated config constants

--- InternetBulletin.Shared/Constants/ConfigurationConstants.cs
+++ InternetBulletin.Shared/Constants/ConfigurationConstants.cs
@@ -7,98 +7,121 @@
 
 namespace InternetBulletin.Shared.Constants
 {
-    /// <summary>
-    /// The Configuration Constants Class.
-    /// </summary>
-    public static class ConfigurationConstants
-    {
-        /// <summary>
-        /// The app configuration endpoint key constant.
-        /// </summary>
-        public const string AppConfigurationEndpointKeyConstant = "AppConfigurationEndpoint";
+	/// <summary>
+	/// The Configuration Constants Class.
+	/// </summary>
+	public static class ConfigurationConstants
+	{
+		/// <summary>
+		/// The app configuration endpoint key constant.
+		/// </summary>
+		public const string AppConfigurationEndpointKeyConstant = "AppConfigurationEndpoint";
 
-        /// <summary>
-        /// The SQL connection string constant
-        /// </summary>
-        public const string SqlConnectionStringConstant = "SqlConnectionString";
+		/// <summary>
+		/// The SQL connection string constant
+		/// </summary>
+		public const string SqlConnectionStringConstant = "SqlConnectionString";
 
-        /// <summary>
-        /// The local sql connection string constant.
-        /// </summary>
-        public const string LocalSqlConnectionStringConstant = "LocalSqlServerConnection";
+		/// <summary>
+		/// The local sql connection string constant.
+		/// </summary>
+		public const string LocalSqlConnectionStringConstant = "LocalSqlServerConnection";
 
-        /// <summary>
-        /// The user assigned client identifier constant
-        /// </summary>
-        public const string ManagedIdentityClientIdConstant = "ManagedIdentityClientId";
+		/// <summary>
+		/// The user assigned client identifier constant
+		/// </summary>
+		public const string ManagedIdentityClientIdConstant = "ManagedIdentityClientId";
 
-        /// <summary>
-        /// The base configuration app config key constant.
-        /// </summary>
-        public const string BaseConfigurationAppConfigKeyConstant = "BaseConfiguration";
+		/// <summary>
+		/// The base configuration app config key constant.
+		/// </summary>
+		public const string BaseConfigurationAppConfigKeyConstant = "BaseConfiguration";
 
-        /// <summary>
-        /// The ibbs api app config key constant.
-        /// </summary>
-        public const string IbbsAPIAppConfigKeyConstant = "IBBS.API";
+		/// <summary>
+		/// The ibbs api app config key constant.
+		/// </summary>
+		public const string IbbsAPIAppConfigKeyConstant = "BulletinAPI";
 
-        /// <summary>
-        /// The ibbs functin app config key constant.
-        /// </summary>
-        public const string IbbsFunctinAppConfigKeyConstant = "IBBS.Function";
+		/// <summary>
+		/// The ibbs functin app config key constant.
+		/// </summary>
+		public const string IbbsFunctinAppConfigKeyConstant = "IBBS.Function";
 
-        /// <summary>
-        /// The local appsettings file constant.
-        /// </summary>
-        public const string LocalAppsettingsFileConstant = "appsettings.development.json";
+		/// <summary>
+		/// The local appsettings file constant.
+		/// </summary>
+		public const string LocalAppsettingsFileConstant = "appsettings.development.json";
 
-        /// <summary>
-        /// The user name claim constant.
-        /// </summary>
-        public const string UserNameClaimConstant = "extension_UserName";
+		/// <summary>
+		/// The user name claim constant.
+		/// </summary>
+		public const string UserNameClaimConstant = "extension_UserName";
 
-        /// <summary>
-        /// The user display name constant.
-        /// </summary>
-        public const string UserDisplayNameConstant = "name";
+		/// <summary>
+		/// The user display name constant.
+		/// </summary>
+		public const string UserDisplayNameConstant = "name";
 
-        /// <summary>
-        /// The azure ad b2 c constant.
-        /// </summary>
-        public const string AzureAdB2CConstant = "AzureAdB2C";
+		#region AzureADB2CConstants
 
-        /// <summary>
-        /// The IBBS api client id constant.
-        /// </summary>
-        public const string IBBSApiClientIdConstant = "AzureAdB2C:ClientId";
+		/// <summary>
+		/// The azure ad b2 c constant.
+		/// </summary>
+		public const string AzureAdB2CConstant = "AzureAdB2C";
 
-        /// <summary>
-        /// The IBBS api issuer constant.
-        /// </summary>
-        public const string IBBSApiIssuerConstant = "AzureAdB2C:Issuer";
+		/// <summary>
+		/// The IBBS api client id constant.
+		/// </summary>
+		public const string IBBSApiClientIdConstant = "AzureAdB2C:ClientId";
 
-        /// <summary>
-        /// The tenant id constant.
-        /// </summary>
-        public const string TenantIdConstant = "AzureAD:TenantId";
+		/// <summary>
+		/// The IBBS api issuer constant.
+		/// </summary>
+		public const string IBBSApiIssuerConstant = "AzureAdB2C:Issuer";
 
-        #region Graph API
+		/// <summary>
+		/// The tenant id constant.
+		/// </summary>
+		public const string TenantIdConstant = "AzureAdB2C:TenantId";
 
-        /// <summary>
-        /// The graph api default scope constant.
-        /// </summary>
-        public const string GraphAPIDefaultScopeConstant = "AzureAD:GraphAPIDefaultScope";
+		#endregion
 
-        /// <summary>
-        /// The graph a p i client id constant.
-        /// </summary>
-        public const string GraphAPIClientIdConstant = "AzureAD:GraphAPIClientId";
+		#region Graph API
 
-        /// <summary>
-        /// The graph a p i client secret constant.
-        /// </summary>
-        public const string GraphAPIClientSecretConstant = "AzureAD:GraphAPIClientSecret";
+		/// <summary>
+		/// The graph api default scope constant.
+		/// </summary>
+		public const string GraphAPIDefaultScopeConstant = "AzureAD:GraphAPI:DefaultScope";
 
-        #endregion
-    }
+		/// <summary>
+		/// The graph a p i client id constant.
+		/// </summary>
+		public const string GraphAPIClientIdConstant = "AzureAD:GraphAPI:ClientId";
+
+		/// <summary>
+		/// The graph a p i client secret constant.
+		/// </summary>
+		public const string GraphAPIClientSecretConstant = "AzureAD:GraphAPI:ClientSecret";
+
+		#endregion
+
+		#region IBBS.AI
+
+		/// <summary>
+		/// The ibbs ai API URL constant
+		/// </summary>
+		public const string IbbsAiApiUrlConstant = "IbbsAiApiUrl";
+
+		/// <summary>
+		/// The ibbs ai ad client id.
+		/// </summary>
+		public const string IbbsAiAdClientId = "AzureAD:IBBS.AI:ClientId";
+
+		/// <summary>
+		/// The ibbs ai ad client secret.
+		/// </summary>
+		public const string IbbsAiAdClientSecret = "AzureAD:IBBS.AI:ClientSecret";
+
+		#endregion
+	}
 }
